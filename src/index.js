68f5791c6c6d2d28d363d6217e8e85cb683057f2
uma899 <umamashesh006@gmail.com>
2025-01-09 18:44:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ export default function Index() {
 return (
 <BrowserRouter>
     <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Login />} />
         <Route path="/*" element={<Notfound />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
