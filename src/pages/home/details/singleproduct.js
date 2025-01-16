bf2dd058660a2fbcd233a9d381588e754c09afed
uma899 <umamashesh006@gmail.com>
2025-01-16 12:50:32 +0530

    Update singleproduct.js

--- src/pages/home/details/singleproduct.js
+++ src/pages/home/details/singleproduct.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './sp.css'
+import { API } from "../../../contants";
 function SingleProduct({product}) {
   return (
     <div className='main-single'>
@@ -7,6 +8,7 @@ function SingleProduct({product}) {
         <p>Name: {product.name}</p>
         <p>Price: {product.price}</p>
         <p>Price: {product.image}</p>
+    <img src= {API + 'images/' + product.image}
         <p>Brand: <a>{product.unit}</a></p>
     </div>
   )
